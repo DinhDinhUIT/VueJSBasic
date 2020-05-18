dece5c543ebd7085876d75579d98f51fdc900f9a
JWAT <JWAT@DDES-0083.clv.local>
2020-05-18 08:59:34 +0700

    project vue js

---
+++ src/components/UserDetail.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="component">
+    <h3>Information User Details</h3>
+    <p>List Details</p>
+    <p>My name: {{ reverseName() }}</p>
+    <button @click="resetName">Reset Name</button>
+    <button @click="resetNameFn">Call back</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    nameTranfer: {
+        type: String,
+        required: false
+    },
+    resetNameFn: Function
+  },
+
+  methods: {
+      reverseName: function(){
+          console.log(this.nameTranfer);
+          return this.nameTranfer;
+      },
+      resetName: function(){
+          this.nameTranfer="Dinh Dinh";
+          this.$emit('nameWasReset', this.nameTranfer);
+      }
+  }
+}
+</script>
+
+<style scoped>
+    div{
+        background-color: lightcoral;
+    }
+</style>
---
+++ src/components/UserEdit.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="component">
+    <h3>Information User Details</h3>
+    <p>List Details</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+    div{
+        background-color: #1dd1a1;
+    }
+</style>
